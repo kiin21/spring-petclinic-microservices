f6987efd135a2c62f195705af48d56d8e22ecc21
Pham Dao Anh Khoa <139786046+kiin21@users.noreply.github.com>
2025-04-11 18:51:37 +0700

    Update CustomersServiceApplication.java

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -25,7 +25,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CustomersServiceApplication {
-    // I was there.
+    // I was there
 	public static void main(String[] args) {
 		SpringApplication.run(CustomersServiceApplication.class, args);
 	}
